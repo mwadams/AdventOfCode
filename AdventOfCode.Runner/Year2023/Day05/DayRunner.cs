2d4a8e0768550b272749527677c91fb503a699cc
Matthew Adams <matthew.adams@endjin.com>
2023-12-05 12:47:53 +0000

    Correct solution for Part2.

--- AdventOfCode.Runner/Year2023/Day05/DayRunner.cs
+++ AdventOfCode.Runner/Year2023/Day05/DayRunner.cs
@@ -1,6 +1,7 @@
 ﻿namespace AdventOfCode.Runner.Year2023.Day05
 {
     using AdventOfCode.Common;
+    using System;
     using System.Threading.Tasks;
 
     public class DayRunner : IDay<DayRunner>
@@ -75,7 +76,7 @@
 
             long result = long.MaxValue;
 
-            foreach(var seed in seeds)
+            foreach (var seed in seeds)
             {
                 long soil = Mapper.GetDestination(seedToSoil, seed);
                 long fertilizer = Mapper.GetDestination(soilToFertilizer, soil);
@@ -97,8 +98,138 @@
         public void SolvePart2<TFormatter>(TFormatter formatter)
             where TFormatter : IResultFormatter
         {
-            long result = 0;
+            ReadOnlySpan<string> lineSpan = lines.AsSpan();
+
+            // Max of 20 seeds
+            Span<(long Lower, long Range)> seedPairsBuffer = stackalloc (long, long)[10];
+
+            int offset = 0;
+
+            int numberOfSeedPairs = ParseSeedPairs(lineSpan[offset], seedPairsBuffer);
+            ReadOnlySpan<(long Lower, long Range)> seedPairs = seedPairsBuffer[..numberOfSeedPairs];
+
+            offset += 3;
+
+            Span<Mapper> seedToSoilBuffer = stackalloc Mapper[45];
+            int numberOfSeedToSoil = ParseBuffer(lineSpan[offset..], seedToSoilBuffer);
+            ReadOnlySpan<Mapper> seedToSoil = seedToSoilBuffer[..numberOfSeedToSoil];
+
+            offset += 2 + numberOfSeedToSoil;
+
+            Span<Mapper> soilToFertilizerBuffer = stackalloc Mapper[45];
+            int numberOfSoilToFertilizer = ParseBuffer(lineSpan[offset..], soilToFertilizerBuffer);
+            ReadOnlySpan<Mapper> soilToFertilizer = soilToFertilizerBuffer[..numberOfSoilToFertilizer];
+
+            offset += 2 + numberOfSoilToFertilizer;
+
+            Span<Mapper> fertilizerToWaterBuffer = stackalloc Mapper[45];
+            int numberOfFertilizerToWater = ParseBuffer(lineSpan[offset..], fertilizerToWaterBuffer);
+            ReadOnlySpan<Mapper> fertilizerToWater = fertilizerToWaterBuffer[..numberOfFertilizerToWater];
+
+            offset += 2 + numberOfFertilizerToWater;
+
+            Span<Mapper> waterToLightBuffer = stackalloc Mapper[45];
+            int numberOfWaterToLight = ParseBuffer(lineSpan[offset..], waterToLightBuffer);
+            ReadOnlySpan<Mapper> waterToLight = waterToLightBuffer[..numberOfWaterToLight];
+
+            offset += 2 + numberOfWaterToLight;
+
+            Span<Mapper> lightToTemperatureBuffer = stackalloc Mapper[45];
+            int numberOfLightToTemperature = ParseBuffer(lineSpan[offset..], lightToTemperatureBuffer);
+            ReadOnlySpan<Mapper> lightToTemperature = lightToTemperatureBuffer[..numberOfLightToTemperature];
+
+            offset += 2 + numberOfLightToTemperature;
+
+            Span<Mapper> temperatureToHumidityBuffer = stackalloc Mapper[45];
+            int numberOfTemperatureToHumidity = ParseBuffer(lineSpan[offset..], temperatureToHumidityBuffer);
+            ReadOnlySpan<Mapper> temperatureToHumidity = temperatureToHumidityBuffer[..numberOfTemperatureToHumidity];
+
+            offset += 2 + numberOfTemperatureToHumidity;
+
+            Span<Mapper> humidityToLocationBuffer = stackalloc Mapper[45];
+            int numberOfHumidityToLocation = ParseBuffer(lineSpan[offset..], humidityToLocationBuffer);
+            ReadOnlySpan<Mapper> humidityToLocation = humidityToLocationBuffer[..numberOfHumidityToLocation];
+
+            long result = long.MaxValue;
+
+            Span<(long Lower, long Range)> accumulator1 = stackalloc (long, long)[45];
+            Span<(long Lower, long Range)> accumulator2 = stackalloc (long, long)[45];
+            Span<(long Lower, long Range)> accumulator3 = stackalloc (long, long)[45];
+            Span<(long Lower, long Range)> accumulator4 = stackalloc (long, long)[45];
+            Span<(long Lower, long Range)> accumulator5 = stackalloc (long, long)[45];
+            Span<(long Lower, long Range)> accumulator6 = stackalloc (long, long)[45];
+            Span<(long Lower, long Range)> accumulator7 = stackalloc (long, long)[45];
+
+            foreach (var seedPair in seedPairs)
+            {
+                int soil = Mapper.GetDestinationRange(seedToSoil, seedPair, accumulator1);
+                foreach (var soilPair in accumulator1[..soil])
+                {
+                    int fertilizer = Mapper.GetDestinationRange(soilToFertilizer, soilPair, accumulator2);
+                    foreach (var fertilizerPair in accumulator2[..fertilizer])
+                    {
+                        int water = Mapper.GetDestinationRange(fertilizerToWater, fertilizerPair, accumulator3);
+                        foreach (var waterPair in accumulator3[..water])
+                        {
+                            int light = Mapper.GetDestinationRange(waterToLight, waterPair, accumulator4);
+
+                            foreach (var lightPair in accumulator4[..light])
+                            {
+                                int temperature = Mapper.GetDestinationRange(lightToTemperature, lightPair, accumulator5);
+
+                                foreach (var temperaturePair in accumulator5[..temperature])
+                                {
+                                    int humidity = Mapper.GetDestinationRange(temperatureToHumidity, temperaturePair, accumulator6);
+
+                                    foreach (var humidityPair in accumulator6[..humidity])
+                                    {
+                                        int location = Mapper.GetDestinationRange(humidityToLocation, humidityPair, accumulator7);
+                                        foreach(var locationPair in accumulator7[..location])
+                                        {
+                                            if (locationPair.Lower < result)
+                                            {
+                                                result = locationPair.Lower;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
             formatter.Format(result);
+        }
+
+
+        private int ParseSeedPairs(ReadOnlySpan<char> line, Span<(long Lower, long Range)> seedsBuffer)
+        {
+            int offset = 7;
+            int index = line[offset..].IndexOf(' ');
+            int outputIndex = 0;
+
+            long lower = -1;
+            while (index != -1)
+            {
+                if (lower == -1)
+                {
+                    lower = long.Parse(line[offset..(offset + index)]);
+                }
+                else
+                {
+                    seedsBuffer[outputIndex++] = (lower, long.Parse(line[offset..(offset + index)]));
+                    lower = -1;
+                }
+
+                offset += index + 1;
+                index = line[offset..].IndexOf(' ');
+            }
+
+            // And the last one
+            seedsBuffer[outputIndex++] = (lower, long.Parse(line[offset..]));
+
+            return outputIndex;
         }
 
         private int ParseSeeds(ReadOnlySpan<char> line, Span<long> seedsBuffer)
--- AdventOfCode.Runner/Year2023/Day05/Mapper.cs
+++ AdventOfCode.Runner/Year2023/Day05/Mapper.cs
@@ -16,7 +16,7 @@ public readonly struct Mapper
 
     public static long GetDestination(ReadOnlySpan<Mapper> map, long input)
     {
-        for(int i = 0; i < map.Length; i++)
+        for (int i = 0; i < map.Length; i++)
         {
             Mapper mapper = map[i];
             var destination = mapper.GetDestination(input);
@@ -27,6 +27,66 @@ public readonly struct Mapper
         }
 
         return input;
+    }
+
+    public static int GetDestinationRange(ReadOnlySpan<Mapper> map, in (long Lower, long Range) input, Span<(long Lower, long Range)> output)
+    {
+        int count = 0;
+        long lowest = input.Lower + input.Range;
+        long highest = input.Lower - 1;
+
+        for (int i = 0; i < map.Length; ++i)
+        {
+            Mapper mapper = map[i];
+            (long lowerOverlap, long upperOverlap) = mapper.GetOverlap(input);
+            if (lowerOverlap >= 0)
+            {
+                var lower = mapper.GetDestination(lowerOverlap);
+                var upper = mapper.GetDestination(upperOverlap);
+
+                output[count++] = (lower, (upper - lower) + 1);
+                if (lowerOverlap < lowest)
+                {
+                    lowest = lowerOverlap;
+                }
+                if (upperOverlap > highest)
+                {
+                    highest = upperOverlap;
+                }
+            }
+        }
+
+        // Add the "outside the range" items that were not found anywhere
+
+        if (lowest > input.Lower)
+        {
+            (long Lower, long Range) range = (input.Lower, lowest - input.Lower);
+            output[count++] = range;
+            if (highest < range.Lower + range.Range - 1)
+            {
+                highest = range.Lower + range.Range - 1;
+            }
+        }
+
+        if (highest < input.Lower + input.Range - 1)
+        {
+            output[count++] = (highest + 1, (input.Lower + input.Range) - (highest + 1));
+        }
+
+        return count;
+    }
+
+    private (long lowerOverlap, long upperOverlap) GetOverlap((long Lower, long Range) input)
+    {
+
+        if (input.Lower < (this.Source + this.Range) && (input.Lower + input.Range -1) > this.Source)
+        {
+            long lowerOverlap = Math.Max(input.Lower, this.Source);
+            long upperOverlap = Math.Min(input.Lower + input.Range - 1, this.Source + this.Range - 1);
+            return (lowerOverlap, upperOverlap);
+        }
+
+        return (-1, -1);
     }
 
     public override string ToString()
