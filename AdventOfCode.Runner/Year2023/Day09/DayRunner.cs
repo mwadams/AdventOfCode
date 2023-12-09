eb2cd29617a56c5f2a735ccc57fe63e8cef9961c
Matthew Adams <matthew.adams@endjin.com>
2023-12-09 06:28:04 +0000

    Part 2 correct, some optimizations applied.

--- AdventOfCode.Runner/Year2023/Day09/DayRunner.cs
+++ AdventOfCode.Runner/Year2023/Day09/DayRunner.cs
@@ -21,15 +21,100 @@
         public void SolvePart1<TFormatter>(TFormatter formatter)
             where TFormatter : IResultFormatter
         {
-            long result = 0;
+            long result =
+                AccumulateResult(
+                    static (result, diffs) =>
+                    {
+                        long current = diffs[^1][^1];
+
+                        for (var i = diffs.Count - 2; i >= 0; i--)
+                        {
+                            // Diff the diffs
+                            current = diffs[i][^1] + current;
+                        }
+
+                        return result + current;
+                    });
+
             formatter.Format(result);
         }
 
         public void SolvePart2<TFormatter>(TFormatter formatter)
             where TFormatter : IResultFormatter
         {
-            long result = 0;
+            long result =
+                AccumulateResult(
+                    static (result, diffs) =>
+                    {
+                        long current = diffs[^1][0];
+
+                        for (var i = diffs.Count - 2; i >= 0; i--)
+                        {
+                            // Diff the diffs
+                            current = diffs[i][0] - current;
+                        }
+
+                        return result + current;
+                    });
+
             formatter.Format(result);
+        }
+
+        private bool HasNonZeroes(List<long> currentNumbers)
+        {
+            return currentNumbers.Any(v => v != 0);
+        }
+
+        private long AccumulateResult(Func<long, List<List<long>>, long> diffMunger)
+        {
+            long result = 0;
+
+            foreach (ReadOnlySpan<char> line in lines)
+            {
+                List<long> currentNumbers = ParseLine(line);
+                List<List<long>> diffs = new(currentNumbers.Count) { currentNumbers };
+                while (HasNonZeroes(currentNumbers))
+                {
+                    var lastDiff = currentNumbers;
+                    currentNumbers = [];
+                    for (int i = 0; i < lastDiff.Count - 1; i++)
+                    {
+                        currentNumbers.Add(lastDiff[i + 1] - lastDiff[i]);
+                    }
+
+                    diffs.Add(currentNumbers);
+                }
+
+                result = diffMunger(result, diffs);
+            }
+
+            return result;
+        }
+
+        private List<long> ParseLine(ReadOnlySpan<char> line)
+        {
+            List<long> result = [];
+
+            int start = 0;
+            int end = 1;
+            while (end < line.Length)
+            {
+                if (line[end] == ' ')
+                {
+                    result.Add(long.Parse(line[start..end]));
+                    start = end + 1;
+                    end = start + 1;
+                }
+                else
+                {
+                    end++;
+                }
+            }
+
+            // And add the one at the end
+            result.Add(long.Parse(line[start..]));
+
+            return result;
         }
     }
 }
