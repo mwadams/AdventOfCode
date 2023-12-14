93a9282850578e8bddccbf526109ca7a1d3ff4da
Matthew Adams <matthew.adams@endjin.com>
2023-12-14 05:45:57 +0000

    Part 1 working.

--- AdventOfCode.Runner/Year2023/Day14/DayRunner.cs
+++ AdventOfCode.Runner/Year2023/Day14/DayRunner.cs
@@ -22,6 +22,15 @@
             where TFormatter : IResultFormatter
         {
             long result = 0;
+
+            Span<Tile> map = stackalloc Tile[lines.Length * lines[0].Length];
+
+            BuildMap(lines, map);
+
+            result = TiltNorthAndCalculateLoad(map, lines[0].Length, lines.Length);
+
+            Dump(map, lines[0].Length, lines.Length);
+
             formatter.Format(result);
         }
 
@@ -31,5 +40,85 @@
             long result = 0;
             formatter.Format(result);
         }
+
+        private void Dump(Span<Tile> map, int width, int height)
+        {
+            for(int y = 0; y < height; ++y)
+            {
+                int yOffset = y * height;
+                for(int x = 0; x < width; ++x)
+                {
+                    Console.Write(map[yOffset + x] switch
+                    {
+                        Tile.Empty => '.',
+                        Tile.Cube => '#',
+                        Tile.Sphere => 'O',
+                        _ => throw new NotImplementedException("Unknown tile"),
+                    });
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private long TiltNorthAndCalculateLoad(Span<Tile> map, int width, int height)
+        {
+            long load = 0;
+            Span<int> targetYOffset = stackalloc int[width];
+            for(int y = 0; y < height; y++)
+            {
+                int yOffset = y * width;
+                for(int x = 0; x < width; x++)
+                {
+                    int offset = yOffset + x;
+                    Tile tile = map[offset];
+                    if (tile == Tile.Sphere)
+                    {
+                        int targetY = targetYOffset[x];
+                        if (targetY != y)
+                        {
+                            map[offset] = Tile.Empty;
+                            map[targetY * width + x] = Tile.Sphere;
+                        }
+
+                        targetYOffset[x] += 1;
+                        load += height - targetY;
+                    }
+                    else if (tile == Tile.Cube)
+                    {
+                        targetYOffset[x] = y + 1;
+                    }
+                }
+            }
+
+            return load;
+        }
+
+        private static void BuildMap(ReadOnlySpan<string> lines, Span<Tile> map)
+        {
+            int offset = 0;
+
+            foreach(ReadOnlySpan<char> line in lines)
+            {
+                foreach(char tile in line)
+                {
+                    map[offset++] = tile switch
+                    {
+                        '.' => Tile.Empty,
+                        '#' => Tile.Cube,
+                        'O' => Tile.Sphere,
+                        _ => throw new NotImplementedException("Unknown tile"),
+                    };
+                }
+            }
+        }
+
+        private enum Tile
+        {
+            Empty,
+            Cube,
+            Sphere,
+        }
+
     }
 }
