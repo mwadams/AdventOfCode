a4841337fbe3ff27613475d6ea24ba36219549be
Matthew Adams <matthew.adams@endjin.com>
2023-12-14 16:45:31 +0000

    Missing day 24!

---
+++ AdventOfCode.Benchmarks/Year2022/Year2022Day24Benchmark.cs
@@ -0,0 +1,7 @@
+﻿namespace AdventOfCode.Benchmarks.Year2022;
+
+using Runner.Year2022.Day24;
+
+public class Year2022Day24Benchmark : BenchmarkBase<DayRunner>
+{
+}
---
+++ AdventOfCode.Benchmarks/Year2023/Year2023Day24Benchmark.cs
@@ -0,0 +1,7 @@
+﻿namespace AdventOfCode.Benchmarks.Year2023;
+
+using Runner.Year2023.Day24;
+
+public class Year2023Day24Benchmark : BenchmarkBase<DayRunner>
+{
+}
