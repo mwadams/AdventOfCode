34909130b12496fd412150e7aba2bbdd2aa99df9
Matthew Adams <matthew.adams@endjin.com>
2023-12-14 07:51:15 +0000

    Stash the cycle, rather than re-calculating the cycles we need.

--- AdventOfCode.Runner/Year2023/Day14/DayRunner.cs
+++ AdventOfCode.Runner/Year2023/Day14/DayRunner.cs
@@ -61,51 +61,48 @@
                 TiltSouthAndCalculateLoad(map, lines[0].Length, lines.Length);
                 result = TiltEastAndCalculateLoad(map, lines[0].Length, lines.Length);
 
-                if (seenResultCount != 0 && seenResults[..seenResultCount].Contains(result))
+                if (!cycled)
                 {
-                    if (!cycled)
+                    if (seenResultCount != 0 && seenResults[..seenResultCount].Contains(result))
                     {
+                        seenResultCount = 0;
                         cycled = true;
                         startOfCycle = i;
+                        seenResults[seenResultCount++] = result;
                         MD5.HashData(MemoryMarshal.AsBytes(map), cycleHash);
                     }
                     else
                     {
-                        MD5.HashData(MemoryMarshal.AsBytes(map), nextHash);
-                        if (cycleHash.SequenceEqual(nextHash))
-                        {
-                            nextCycle = i;
-                            break;
-                        }
+                        seenResults[seenResultCount++] = result;
                     }
                 }
                 else
                 {
                     seenResults[seenResultCount++] = result;
+                    MD5.HashData(MemoryMarshal.AsBytes(map), nextHash);
+                    if (cycleHash.SequenceEqual(nextHash))
+                    {
+                        nextCycle = i;
+                        break;
+                    }
                 }
             }
 
             int consumedAtStart = startOfCycle;
 
-            int additionalCyclesRequired = ((1000000000 - consumedAtStart) % (nextCycle - startOfCycle)) - 1;
-            
-            for(int i =0; i < additionalCyclesRequired; ++i)
-            {
-                TiltNorthAndCalculateLoad(map, lines[0].Length, lines.Length);
-                TiltWestAndCalculateLoad(map, lines[0].Length, lines.Length);
-                TiltSouthAndCalculateLoad(map, lines[0].Length, lines.Length);
-                result = TiltEastAndCalculateLoad(map, lines[0].Length, lines.Length);
-            }
+            int cycleIndex = ((1000000000 - consumedAtStart) % (nextCycle - startOfCycle)) - 1;
+
+            result = seenResults[cycleIndex];
 
             formatter.Format(result);
         }
 
         private void Dump(Span<Tile> map, int width, int height)
         {
-            for(int y = 0; y < height; ++y)
+            for (int y = 0; y < height; ++y)
             {
                 int yOffset = y * height;
-                for(int x = 0; x < width; ++x)
+                for (int x = 0; x < width; ++x)
                 {
                     Console.Write(map[yOffset + x] switch
                     {
@@ -126,10 +123,10 @@
         {
             int load = 0;
             Span<int> targetYOffset = stackalloc int[width];
-            for(int y = 0; y < height; y++)
+            for (int y = 0; y < height; y++)
             {
                 int yOffset = y * width;
-                for(int x = 0; x < width; x++)
+                for (int x = 0; x < width; x++)
                 {
                     int offset = yOffset + x;
                     Tile tile = map[offset];
@@ -260,9 +257,9 @@
         {
             int offset = 0;
 
-            foreach(ReadOnlySpan<char> line in lines)
+            foreach (ReadOnlySpan<char> line in lines)
             {
-                foreach(char tile in line)
+                foreach (char tile in line)
                 {
                     map[offset++] = tile switch
                     {
