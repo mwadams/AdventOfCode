37a0e9a610cf0d6c046df3b95b717256ca40efff
Matthew Adams <matthew.adams@endjin.com>
2023-12-18 10:24:45 +0000

    Working pt 2

--- AdventOfCode.Runner/Year2023/Day18/DayRunner.cs
+++ AdventOfCode.Runner/Year2023/Day18/DayRunner.cs
@@ -49,7 +49,62 @@
             where TFormatter : IResultFormatter
         {
             long result = 0;
+
+            Span<long> xCoords = stackalloc long[lines.Length + 1];
+            Span<long> yCoords = stackalloc long[lines.Length + 1];
+
+            result = (BuildCoordinates(lines, xCoords, yCoords) / 2L) + 1;
+            result += ShoelaceFormula(xCoords, yCoords);
+
             formatter.Format(result);
+        }
+
+        private static long ShoelaceFormula(ReadOnlySpan<long> x, ReadOnlySpan<long> y)
+        {
+            long area = 0;
+            int j = x.Length - 1;
+            for (int i = 0; i < x.Length; i++)
+            {
+                area += (x[j] + x[i]) * (y[j] - y[i]);
+                j = i;
+            }
+            return Math.Abs(area / 2L);
+        }
+
+        private static long BuildCoordinates(ReadOnlySpan<string> lines, Span<long> xCoords, Span<long> yCoords)
+        {
+            int offset = 1; // We need a (0,0) to start with
+            long count = 0;
+            long x = 0;
+            long y = 0;
+
+            foreach (var line in lines)
+            {
+                long distance = long.Parse(line[(line.IndexOf('#') + 1)..^2], System.Globalization.NumberStyles.HexNumber);
+                int direction = line[^2];
+                count += Math.Abs(distance);
+                switch (direction)
+                {
+                    case '0':
+                        x += distance;
+                        break;
+                    case '1':
+                        y += distance;
+                        break;
+                    case '2':
+                        x -= distance;
+                        break;
+                    case '3':
+                        y -= distance;
+                        break;
+                }
+
+                xCoords[offset] = x;
+                yCoords[offset] = y;
+                offset++;
+            }
+
+            return count;
         }
 
         private static (int x, int y) FindSeed(Span<int> map, int width, int height)
